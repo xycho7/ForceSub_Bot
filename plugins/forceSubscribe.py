169695a9ddc3090767564c0b85883b0a778eb43c
xycho7 <112860174+xycho7@users.noreply.github.com>
2022-09-17 22:21:08 +0530

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -37,7 +37,7 @@ async def _onUnMuteRequest(client, cb):
 
 
 
-@Client.on_edited_message((filters.text | filters.media) & ~filters.private & ~filters.edited, group=1)
+@Client.on_message((filters.text | filters.media) & ~filters.private , group=1)
 async def _check_member(client, message):
   chat_id = message.chat.id
   chat_db = sql.fs_settings(chat_id)
